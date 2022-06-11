5b97828891e4104a4d2e130ff6c2ffe1bddcd0ed
Tristan Montoya <montoya.tristan@gmail.com>
2022-06-11 17:23:25 -0400

    ran p5 p10 nodal

--- scripts/submit_collapsed_nodal_hrefine.sh
+++ scripts/submit_collapsed_nodal_hrefine.sh
@@ -2,23 +2,19 @@
 
 #SBATCH --nodes=1
 #SBATCH --ntasks-per-node=40
-#SBATCH --time=12:00:00
-#SBATCH --job-name collapsed_nodal_hrefine
+#SBATCH --time=24:00:00
+#SBATCH --job-name collapsed_nodal_hrefine_p10
 
-module load NiaEnv/2019b julia/1.7 python/3.8
+module load NiaEnv/2019b julia/1.7
 
 source ~/.virtualenvs/tristan/bin/activate
 
-p=2
+p=10
 scheme=CollapsedSEM
 form=SplitConservationForm
 
 
 cd /home/z/zingg/tmontoya/scratch/CLOUD.jl/drivers
 
-
-for M in 2 4 8 16
-do
-	julia --project=.. advection_2d.jl -p $p -M $M -s $scheme -f $form > out.txt &
-done
+julia --project=.. --threads 16 advection_2d.jl -p $p -r $p -M 2 -g 4 -s $scheme -f $form > out.txt
 
