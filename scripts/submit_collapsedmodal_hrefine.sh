4ce52a817f30dcce76877736c4bd18d088f7f687
Tristan Montoya <montoya.tristan@gmail.com>
2022-06-16 16:15:58 -0400

    edit scripts

--- scripts/submit_collapsedmodal_hrefine.sh
+++ scripts/submit_collapsedmodal_hrefine.sh
@@ -15,7 +15,7 @@ form=SplitConservationForm
 
 cd /home/z/zingg/tmontoya/scratch/CLOUD.jl/drivers
 
-julia --project=.. --threads 4 advection_2d.jl -b 0.005 -m 0.2 -p 4 -r 4 -l 0.0 -M 16 -g 4 -s $scheme -f $form &
-julia --project=.. --threads 4 advection_2d.jl -b 0.005 -m 0.2 -p 4 -r 4 -l 1.0 -M 16 -g 4 -s $scheme -f $form &
-julia --project=.. --threads 16 advection_2d.jl -b 0.005 -m 0.2 -p 9 -r 9 -l 0.0 -M 16 -g 4 -s $scheme -f $form &
-julia --project=.. --threads 16 advection_2d.jl -b 0.005 -m 0.2 -p 9 -r 9 -l 1.0 -M 16 -g 4 -s $scheme -f $form
+julia --project=.. --threads 4 advection_2d.jl -b 0.005 -m 0.2 -p 4 -r 4 -l 0.0 -M 2 -g 6 -s $scheme -f $form &
+julia --project=.. --threads 4 advection_2d.jl -b 0.005 -m 0.2 -p 4 -r 4 -l 1.0 -M 2 -g 6 -s $scheme -f $form &
+julia --project=.. --threads 16 advection_2d.jl -b 0.005 -m 0.2 -p 9 -r 9 -l 0.0 -M 2 -g 6 -s $scheme -f $form &
+julia --project=.. --threads 16 advection_2d.jl -b 0.005 -m 0.2 -p 9 -r 9 -l 1.0 -M 2 -g 6 -s $scheme -f $form
