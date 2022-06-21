a7217920d08f3394d10cce9873db4fbe1661652d
Tristan Montoya <montoya.tristan@gmail.com>
2022-06-20 21:07:14 -0400

    separated upwind/central scripts

---
+++ scripts/submit_collapsedsem_hrefine_upwind.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+#SBATCH --nodes=1
+#SBATCH --ntasks-per-node=40
+#SBATCH --time=24:00:00
+#SBATCH --job-name collapsedsem_hrefine_upwind
+
+module load NiaEnv/2019b julia/1.7
+
+export OMP_NUM_THREADS=1
+export OPENBLAS_NUM_THREADS=1
+export MPLCONFIGDIR=/home/z/zingg/tmontoya/scratch/CLOUD.jl/.mplconfig/
+
+scheme=CollapsedSEM
+form=SplitConservationForm
+
+cd /home/z/zingg/tmontoya/scratch/CLOUD.jl/drivers
+
+julia --project=.. --threads 4 advection_2d.jl -C 0.0004 -m 0.2 -p 4 -r 4 -l 1.0 -M 2 -g 6 -s $scheme -f $form -i DP8 -n 50 &
+julia --project=.. --threads 32 advection_2d.jl -C 0.0004 -m 0.2 -p 9 -r 9 -l 1.0 -M 2 -g 6 -s $scheme -f $form -i DP8 -n 50
