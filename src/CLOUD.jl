3862295bdd6094a9fb3d3780e620439d4c237596
Tristan Montoya <montoya.tristan@gmail.com>
2022-06-29 15:37:30 -0400

    streamlined timer functionality

--- src/CLOUD.jl
+++ src/CLOUD.jl
@@ -29,8 +29,19 @@ module CLOUD
     include("Analysis/Analysis.jl")
     @reexport using .Analysis
 
-    using TimerOutputs: get_timer
-    for i in 1:Threads.nthreads()
-        to = get_timer(string("thread_timer_",i))
+    using TimerOutputs
+
+    function CLOUD_reset_timer()
+        for t in 1:Threads.nthreads()
+            reset_timer!(get_timer(string("thread_timer_",t)))
+        end
     end
+
+    function CLOUD_print_timer()
+        to = merge(Tuple(get_timer(string("thread_timer_",t)) 
+        for t in 1:Threads.nthreads())...)
+        print_timer(to)
+    end
+    
+    export CLOUD_reset_timer, CLOUD_print_timer
 end
