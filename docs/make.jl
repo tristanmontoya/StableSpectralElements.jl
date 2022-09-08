1c7b3a1b8fc7e7255ed7d9e5cd37bf7b765a2dd4
Tristan Montoya <montoya.tristan@gmail.com>
2022-09-08 10:50:44 -0400

    deploy docs

--- docs/make.jl
+++ docs/make.jl
@@ -3,6 +3,6 @@ using Documenter
 
 makedocs(sitename="CLOUD.jl")
 
-deploydocs(
-    repo = "github.com/USER_NAME/PACKAGE_NAME.jl.git",
+deploydocs(#
+    repo="github.com/tristanmontoya/CLOUD.jl.git"
 )
