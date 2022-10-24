dd8ba51fcaf3f749c9d82b1e7d950297e77967fc
Tristan Montoya <montoya.tristan@gmail.com>
2022-10-24 14:14:55 -0400

    cleanup

--- src/MatrixFreeOperators/MatrixFreeOperators.jl
+++ src/MatrixFreeOperators/MatrixFreeOperators.jl
@@ -2,7 +2,7 @@ module MatrixFreeOperators
 
     using LinearAlgebra, LinearMaps, MuladdMacro, UnPack
 
-    export AbstractOperatorAlgorithm, BLASAlgorithm, GenericMatrixAlgorithm, DefaultOperatorAlgorithm, combine, make_operator
+    export AbstractOperatorAlgorithm, BLASAlgorithm, GenericMatrixAlgorithm, DefaultOperatorAlgorithm, combine, make_operator, count_ops
     
     abstract type AbstractOperatorAlgorithm end
     struct BLASAlgorithm <: AbstractOperatorAlgorithm end
@@ -25,6 +25,17 @@ module MatrixFreeOperators
         return LinearMap(convert(Matrix,map))
     end
     
+    function count_ops(map::LinearMap)
+        x = rand(size(map,2))
+        y = rand(size(map,1))
+        cnt = @count_ops mul!(y,map,x)
+        return cnt.muladd64 + cnt.add64 + cnt.mul64
+    end
+
+    ```
+    Remove unnecessary add operations in diagonal matrix multiplication
+    See Julia issue 47312 at https://github.com/JuliaLang/julia/issues/47312
+    ```
     function LinearAlgebra.mul!(y::AbstractVector, 
         A::Diagonal, x::AbstractVector)
         y[:] = A.diag .* x
