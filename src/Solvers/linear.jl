29e42b536c6e795ac14af50093e4428512402b69
Tristan Montoya <montoya.tristan@gmail.com>
2023-04-23 17:53:00 -0400

    fix LinearResidual

--- src/Solvers/linear.jl
+++ src/Solvers/linear.jl
@@ -1,25 +1,17 @@
 """ Semi-discrete residual operator as a LinearMap"""
 struct LinearResidual <: LinearMap{Float64}
     solver::Solver
-    N_p::Int
-    N_c::Int
-    N_e::Int
 end
 
-Base.size(L::LinearResidual) = (L.N_p*L.N_c*L.N_e, L.N_p*L.N_c*L.N_e)
+Base.size(L::LinearResidual) = (L.solver.N_p*L.solver.N_c*L.solver.N_e, 
+    L.solver.N_p*L.solver.N_c*L.solver.N_e)
 
-function LinearResidual(
-    solver::Solver{ResidualForm,DiscretizationOperators,d}) where {ResidualForm,DiscretizationOperators,d}
-
-    return LinearResidual(solver,solver.operators[1].N_p,
-        solver.conservation_law.N_c,length(solver.operators))
-end
 
 function LinearAlgebra.mul!(y::AbstractVector, 
         L::LinearResidual,
         x::AbstractVector)
-    u = reshape(x,(L.N_p,L.N_c,L.N_e))
-    dudt = Array{Float64}(undef,L.N_p,L.N_c,L.N_e)
+    u = reshape(x,(L.solver.N_p,L.solver.N_c,L.solver.N_e))
+    dudt = Array{Float64}(undef,L.solver.N_p,L.solver.N_c,L.solver.N_e)
     rhs!(dudt,u,L.solver,0.0)
     y[:] = vec(dudt)
     return y
