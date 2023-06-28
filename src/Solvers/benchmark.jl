6785ba9f9edf16671c74f7ed5011acba99390faa
Tristan Montoya <montoya.tristan@gmail.com>
2023-06-28 12:30:46 -0400

    fix benchmarks

--- src/Solvers/benchmark.jl
+++ src/Solvers/benchmark.jl
@@ -1,29 +1,35 @@
 using BenchmarkTools
 
-@inline function rhs_benchmark!(
+@views @timeit "du/dt" function rhs_benchmark!(
     dudt::AbstractArray{Float64,3}, u::AbstractArray{Float64,3}, 
     solver::Solver{d, <:StandardForm, FirstOrder, PhysicalOperators{d},N_p,N_q,N_f,N_c,N_e},
     t::Float64) where {d,N_p,N_q,N_f,N_c,N_e}
 
-    (; conservation_law, operators, connectivity, form) = solver
-    (; inviscid_numerical_flux) = form
-    (; source_term) = conservation_law
-    (; f_q, f_f, u_q, u_f, temp, CI) = solver.preallocated_arrays
+    @timeit "unpack" begin
+        (; conservation_law, operators, connectivity, form) = solver
+        (; inviscid_numerical_flux) = form
+        (; f_q, f_f, u_q, u_f, temp, CI) = solver.preallocated_arrays
+    end
     
     k = 1  # just one element
 
-    mul!(view(u_q, :,:,k), operators.V[k], u[:,:,k])
-    mul!(view(u_f,:,k,:), operators.R[k], u_q[:,:,k])
+    @timeit "vandermonde" mul!(u_q[:,:,k], operators.V[k], u[:,:,k])
+    @timeit "extrap solution" mul!(u_f[:,k,:], operators.R[k], u_q[:,:,k])
 
-    physical_flux!(view(f_q,:,:,:,k),conservation_law, u_q[:,:,k])
+    @timeit "phys flux" physical_flux!(f_q[:,:,:,k],
+        conservation_law, u_q[:,:,k])
 
-    f_f[:,:,k] .= numerical_flux(conservation_law, inviscid_numerical_flux,
-        u_f[:,k,:], u_f[CI[connectivity[:,k]],:], operators.n_f[k])
+    @timeit "num flux" f_f[:,:,k] .= numerical_flux( 
+        conservation_law,
+        inviscid_numerical_flux, u_f[:,k,:], 
+        u_f[CI[connectivity[:,k]],:], operators.n_f[k])
 
-    fill!(view(dudt,:,:,k),0.0)
+    @timeit "fill w zeros" fill!(view(dudt,:,:,k),0.0)
     @inbounds for m in 1:d
-        mul!(view(temp,:,:,k),operators.VOL[k][m],f_q[:,:,m,k])
-        dudt[:,:,k] .+= temp[:,:,k] 
+        @timeit "volume operators" begin
+            mul!(view(temp,:,:,k),operators.VOL[k][m],f_q[:,:,m,k])
+            dudt[:,:,k] .+= temp[:,:,k] 
+        end
     end
     
     mul!(view(temp,:,:,k), operators.FAC[k], f_f[:,:,k])
@@ -32,7 +38,7 @@ using BenchmarkTools
     return dudt
 end
 
-@inline @views @timeit "rhs!" function rhs_benchmark!(
+@views @timeit "du/dt" function rhs_benchmark!(
     dudt::AbstractArray{Float64,3}, u::AbstractArray{Float64,3}, 
     solver::Solver{d, <:StandardForm{SkewSymmetricMapping}, FirstOrder, ReferenceOperators{d},N_p,N_q,N_f,N_c,N_e},
     t::Float64) where {d,N_p,N_q,N_f,N_c,N_e}
@@ -52,12 +58,12 @@ end
     @timeit "phys flux" physical_flux!(f_q[:,:,:,k],
         conservation_law, u_q[:,:,k])
 
-    @timeit "num flux" numerical_flux!(f_f[:,:,k], 
+    @timeit "num flux" f_f[:,:,k] .= numerical_flux( 
         conservation_law,
         inviscid_numerical_flux, u_f[:,k,:], 
         u_f[CI[connectivity[:,k]],:], n_f[k])
 
-    @timeit "fill w zeros" fill!(view(r_q,:,:,k),0.0)
+    @timeit "fill w zeros" fill!(r_q[:,:,k],0.0)
 
     @inbounds for n in 1:d
         @inbounds @timeit "volume operators" for m in 1:d
